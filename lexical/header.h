49407a2ae4a6bda8bb847e4970ed3b92966c187d
Matheus DallRosa <matheusdallrosa94@gmail.com>
2017-03-12 11:40:47 -0300

    Libs for the lexical analysis.

---
+++ lexical/header.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+
+#define sz(v) ((int)(v.size()))
+#define pb push_back
+#define f first
+#define s second
+
+typedef vector<int> vi;
+typedef vi::iterator viIt;
+
+typedef map<char, vi> mcvi;
+typedef mcvi::iterator mcviIt;
+
+typedef map<int, mcvi> AF;
+typedef AF::iterator AFit;
+
+typedef map<int,int>::iterator miiIt;
+
+const int S = 0;
+const char EPS = '#';
